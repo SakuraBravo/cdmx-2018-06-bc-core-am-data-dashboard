c6a7ae9ac2fd0df4c18b5583cabad4aeea345b76
Sakura Bravo <SakuraBravo@MacBook-Pro-de-Nallely.local>
2018-07-03 12:03:26 -0500

    Data y Main actualizados pasando los primeros test

--- src/data.js
+++ src/data.js
@@ -1,29 +1,43 @@
-// let blockData = [];
-// const url = "https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
-// const getJson = () => {
-//   fetch (url)
-//   .then (res => res.json())
-//   .then ((data) => {
-//   blockData.push(data);
-//   console.log(blockData);
-// })
-//   .catch((error) => {
-//     console.log('Huston we have a problem' );
-//   })
-// };
-//
-// getJson();
 window.data = {
  computeStudentsStats : (laboratoria) => {
-         let sedeNames = Object.getOwnPropertyNames(laboratoria);
-         let sedes = Object.values(laboratoria);
-         sedes.forEach(element) => {
-           let generationNames = Object.getOwnPropertyNames(sedes);
-           let generations = Object.values(sedes);
-           console.log(generationNames);
-           console.log(generations);
-           document.getElementById('demo').innerHTML = generationNames;
-         }
+//_____ Aquí van todas las funciones para sacar promedios de las estudiantes
+
+    let sedes = Object.keys(laboratoria); // Sedes ya es un arreglo.
+    //document.getElementById("demo").innerHTML = sedes[2];
+    //console.log(sedes);
+         //let sedeNames = Object.getOwnPropertyNames(laboratoria);
+
+//----Obtengo los campus
+    for (info in laboratoria){
+      //console.log(info)
+      let generations = Object.keys(laboratoria[info].generacion); // Sedes ya es un arreglo.
+
+      //console.log(generations);
+      // generations.forEach(info) => {
+      //   let students = generacion[info].estudiantes;
+      //   console.log(students);
+      // }
+  }
+//console.log(gen);
+
+         //return students = [campus,generacio]
+        // return students = [stats]
+},
+
+computeGenerationsStats : (laboratoria) =>{
+  for(key in laboratoria){
+    let contries = key;
+    let generations = laboratoria[key].generacion;
+    console.log(contries);
+    console.log(generations);
+  }
 
 },
+sortStudents : (laboratoria) => {
+
+},
+
+filterStudents :() => {
+
+  },
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
-let correo = document.getElementById('email');
-let llave = document.getElementById('password');
-document.getElementById('enter').addEventListener('clik', userLaboratoria);
-
-const userLaboratoria = () => {
-   let mail = correo.value;
-   let password = llave.value;
-    if(mail == 'prueba@laboratoria.la' && password == '20181'){
-      return true;
-    } else{
-      alert ('!Usuario No encontrado!')
-    }
-//   console.log(mail);
-//   console.log(password);
-// }
-
-
-//let dataBlock = Object.assign({}, json);
-const getData = () => {
-  const url = "https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
-    fetch (url)
+// let correo = document.getElementById('email');
+// let llave = document.getElementById('password');
+// document.getElementById('enter').addEventListener('clik', userLaboratoria);
+//
+// const userLaboratoria = () => {
+//    let mail = correo.value;
+//    let password = llave.value;
+//     if(mail == 'prueba@laboratoria.la' && password == '20181'){
+//       return true;
+//     } else{
+//       alert ('!Usuario No encontrado!')
+//     }
+//   };
+const url = "https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+const getData = (data) => {
+    fetch (data)
     .then (res => res.json())
     .then ((laboratoria) => {
-    data.computeStudentsStats(laboratoria);
+    //console.log(laboratoria);
+    window.data.computeStudentsStats(laboratoria);
+    window.data.computeGenerationsStats(laboratoria);
+
+
   })
     .catch((error) => {
       console.log('Huston we have a problem' );
     })
   };
-getData();
+getData(url);
