cc8c1880901ed7a132c905645208fa8d0e4ef3a4
ninagallo <sanndsannd@gmail.com>
2018-07-09 12:18:19 -0500

    Creacionde funcion de redondeo global

--- src/data.js
+++ src/data.js
@@ -1,70 +1,96 @@
 const statusP = (num) => {
-    if (num <= 60){
-      return 'Insuficiente'
-    }else if (num <= 89){
-      return 'Suficiente'
-    }else if (num >= 90){
-      return 'Destacado'
-    }else
-    return 'No hay info'
-  }
+  if (num <= 60) {
+    return 'Insuficiente';
+  } else if (num <= 89) {
+    return 'Suficiente';
+  } else if (num >= 90) {
+    return 'Destacado';
+  } else
+    return 'No hay info';
+};
 
 const students = [];
-const generation = [];
+const generations = [];
 let topic;
 let cp;
+const sum = [];
 
 window.data = {
 
-  computeStudentsStats : (laboratoria) => {
-    //_____ Aquí van todas las funciones para sacar promedios de las estudiantes
-    for(key in laboratoria){
+  computeStudentsStats: (laboratoria) => {
+    // Aquí van todas las funciones para sacar promedios de las estudiantes
+    for (key in laboratoria) {
       let campus = key;
-      //console.log(obj.campus);
-      let generations =(laboratoria[key].generacion);
-      //console.log(generations);
-      for (gen in generations){
+      // console.log(obj.campus);
+      let generations = (laboratoria[key].generacion);
+      // console.log(generations);
+      for (gen in generations) {
         let studentsArr = generations[gen].estudiantes;
-        for (topics of studentsArr){
-        topic = topics.progreso.temas;
-          for(item in topic) {
+        for (topics of studentsArr) {
+          topic = topics.progreso.temas;
+          for (item in topic) {
             cp = topic[item];
-            //console.log(topic[item])
+            // console.log(topic[item])
           }
         };
         studentsArr.forEach(student => {
-          //console.log(gen, key);
+          // console.log(gen, key);
           const obj = {
-            name : student.nombre,
+            name: student.nombre,
             email: student.correo,
             campus: key,
-            generation : gen,
-            stats : {
-              status : statusP(student.progreso.porcentajeCompletado),
+            generation: gen,
+            stats: {
+              status: statusP(student.progreso.porcentajeCompletado),
               completedPercentage: student.progreso.porcentajeCompletado,
-              temas : topic,
-              topic : {
-                completedPercentage : cp,
+              temas: topic,
+              topic: {
+                completedPercentage: cp,
               },
             },
-          }
+          };
           students.push(obj);
-        })
+        });
       }
     }
-    //console.log('Array Students', students);
     return students;
   },
 
-  computeGenerationsStats : (laboratoria) =>{
-
-
+  computeGenerationsStats: (laboratoria) =>{
+    for (key in laboratoria) {
+      let campus = key;
+      let generation = laboratoria[key].generacion;
+      for (gen in generation) {
+        let generationsArr = generation[gen].estudiantes;
+        generationsArr.forEach(generation => {
+          let completedPercentageByStudent = generation.progreso.porcentajeCompletado;
+          // console.log(completedPercentageByStudent)
+          sum.push(completedPercentageByStudent);
+          // console.log(completedPercentageByStudent)
+        });
+        // for (item of generationsArr) {
+        //   item = topics.progreso.porcentajeCompletado;
+        //   // console.log(item);
+        //
+        // }
+      }
+      // generations.push({'campus': key,
+      //  'generation': generation,
+      //  'completedPercentage': average,
+      //  });
+    }
+    // console.log(generations);
+    // console.log(generations)
+    let result = sum.reduce((a, b) => a + b) / 134;
+    let average = Math.round(result);
+    console.log(result);
+    console.log(average);
   },
 
-  sortStudents : (students, orderBy, orderDirection) => {
+  sortStudents: (students, orderBy, orderDirection) => {
   },
 
-  filterStudents :(students, search) => {
+  filterStudents: (students, search) => {
 
   },
-}
+};
--- src/main.js
+++ src/main.js
@@ -11,31 +11,23 @@
 //       alert ('!Usuario No encontrado!')
 //     }
 //   };
-const url = "https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+const url = 'https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 const getData = (data) => {
-    fetch (data)
-    .then (res => res.json())
-    .then ((laboratoria) => {
-    //console.log(laboratoria);
-    window.data.computeStudentsStats(laboratoria);
-    //Entrar a la data de cada estudiante para imprimir los valores que queremos en cada una de las secciones
-    students.map(student => {
-      let result = ''
-      let printResult = document.getElementById('btn-lima').addEventListener('click', (event) =>{
-        document.getElementById('demo').innerHTML += `${student.name}<br>`
-      })
-    });
-    console.log(students)
-    drawContries(laboratoria);
-    window.data.computeGenerationsStats(laboratoria);
-    let printStudents = document.getElementById('demo')
-    drawContries(laboratoria);
-
-
-  })
-    .catch((error) => {
-      console.log('Houston we have a problem' );
+  fetch(data)
+    .then(res => res.json())
+    .then((laboratoria) => {
+    // console.log(laboratoria);
+      window.data.computeStudentsStats(laboratoria);
+      window.data.computeGenerationsStats(laboratoria);
     })
-  };
-
+    .catch((error) => {
+      console.log('Houston we have a problem');
+    });
+};
 getData(url);
+
+//console.log(students);
+// drawContries(laboratoria);
+
+// let printStudents = document.getElementById('demo')
+// drawContries(laboratoria);
