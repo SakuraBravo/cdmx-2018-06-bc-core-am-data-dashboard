0b968872ae16185390e5a6a568368b5f431cf76b
Sakura Bravo <SakuraBravo@MacBook-Pro-de-Nallely.local>
2018-07-09 19:19:02 -0500

    Data.js con variables globales corregidas y segunda función

--- src/data.js
+++ src/data.js
@@ -1,69 +1,106 @@
 const statusP = (num) => {
-    if (num <= 60){
-      return 'Insuficiente'
-    }else if (num <= 89){
-      return 'Suficiente'
-    }else if (num >= 90){
-      return 'Destacado'
-    }else
-    return 'No hay info'
-  }
-
-const students = [];
-const generation = [];
+  if (num <= 60) {
+    return 'Insuficiente';
+  } else if (num <= 89) {
+    return 'Suficiente';
+  } else if (num >= 90) {
+    return 'Destacado';
+  } else
+    return 'No hay info';
+};
+// Formula para obtener el promedio general dependiendo de la cantidad de estudiantes que se ingresen
+// const averageFormula = ('sumaDeArray','diviciónEntreEstudiantes.length') => {
+//
+// };
+const suma = [];
 let topic;
 let cp;
+let obj;
+let objeto;
+let result;
 
-window.data = {
 
-  computeStudentsStats : (laboratoria) => {
-    //_____ Aquí van todas las funciones para sacar promedios de las estudiantes
-    for(key in laboratoria){
-      let campus = key;
-      //console.log(obj.campus);
-      let generations =(laboratoria[key].generacion);
-      //console.log(generations);
-      for (gen in generations){
-        let studentsArr = generations[gen].estudiantes;
-        for (topics of studentsArr){
+window.computeStudentsStats = (laboratoria) => {
+  const students = [];
+  for (key in laboratoria) {
+    let campus = key;
+    // console.log(obj.campus);
+    let generations = (laboratoria[key].generacion);
+    // console.log(generations);
+    for (gen in generations) {
+      let studentsArr = generations[gen].estudiantes;
+      for (topics of studentsArr) {
         topic = topics.progreso.temas;
-          for(item in topic) {
-            cp = topic[item];
-          }
-        };
-        studentsArr.forEach(student => {
-          //console.log(gen, key);
-          const obj = {
-            name : student.nombre,
-            email: student.correo,
-            campus: key,
-            generation : gen,
-            stats : {
-              status : statusP(student.progreso.porcentajeCompletado),
-              completedPercentage: student.progreso.porcentajeCompletado,
-              temas : topic,
-              topic : {
-                completedPercentage : cp,
-              },
+        // console.log(topic);
+        for (item in topic) {
+          cp = topic[item];
+          // console.log(cp);
+        }
+      };
+      studentsArr.forEach(student => {
+        // console.log(gen, key);
+        obj = {
+          name: student.nombre,
+          email: student.correo,
+          campus: key,
+          generation: gen,
+          stats: {
+            status: statusP(student.progreso.porcentajeCompletado),
+            completedPercentage: student.progreso.porcentajeCompletado,
+            topics: topic,
+            topic: {
+              completedPercentage: cp,
             },
-          }
-          students.push(obj);
-        })
-      }
+          },
+        };
+        // console.log(student.name)
+        // let arr = obj.stats.completedPercentage;
+        // // console.log(arr);
+        // suma.push(arr);
+        students.push(obj);
+      });
     }
-    console.log('Array Students', students);
-    return students.campus.generacion;
-  },
+  }
+  console.log('Array con Objetos', students);
+  return students;
+};
 
-  computeGenerationsStats : (laboratoria) =>{
+window.computeGenerationsStats = (laboratoria) => {
+  const generation = [];
+  const suma = [];
+  for (key in laboratoria) {
+    let campus = key;
+    let generations = (laboratoria[key].generacion);
+    for (gen in generations) {
+      let generationArr = generations[gen].estudiantes;
+      for (averages of generationArr) {
+        average = averages.progreso.porcentajeCompletado;
+        //   for (numbers in average) {
+        //     result = average[numbers].reduce((i, j) => i + j);
+        //     console.log(result);
+        //   }
+      };
+      generationArr.forEach(item => {
+        objeto = {
+          campus: key,
+          generation: gen,
+          average: '',
+          count: '',
+          // average: averageFormula(item.progreso.porcentajeCompletado, item.length),
+          completedPercentage: item.progreso.porcentajeCompletado,
+        };
+        generation.push(objeto);
+      });
+    }
+  }
+  return generation;
+  // console.log(generation);
+};
 
+window.sortStudents = (students, orderBy, orderDirection) => {
+  // console.log(students);
+};
 
-  },
-
-  sortStudents : (students, orderBy, orderDirection) => {
-  },
-
-  filterStudents :(students, search) => {
-
-  },
-}
+window.filterStudents = (students, search) => {
+  // console.log(students);
+};
--- src/main.js
+++ src/main.js
@@ -11,26 +11,48 @@
 //       alert ('!Usuario No encontrado!')
 //     }
 //   };
-const url = "https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json";
+const url = 'https://raw.githubusercontent.com/SakuraBravo/cdmx-2018-06-bc-core-am-data-dashboard/master/data/laboratoria.json';
 const getData = (data) => {
-    fetch (data)
-    .then (res => res.json())
-    .then ((laboratoria) => {
-    //console.log(laboratoria);
-
-    window.data.computeStudentsStats(laboratoria);
-    window.data.computeGenerationsStats(laboratoria);
-
-
-  })
-    .catch((error) => {
-      console.log('Houston we have a problem' );
+  fetch(data)
+    .then(res => res.json())
+    .then((laboratoria) => {
+      // console.log(laboratoria);
+      const students = window.computeStudentsStats(laboratoria);
+      const generations = window.computeGenerationsStats(laboratoria);
+      // const students = data.computeStudentsStats(laboratoria);
+      window.sortStudents(students);
     })
-  };
+    .catch((error) => {
+      console.log('Houston we have a problem');
+    });
+};
 getData(url);
 
-// Función para el botón super califragilística y espialidoza
-$("#menu-toggle").click(function(e) {
-  e.preventDefault(); 
-$("#wrapper").toggleClass("toggled");
+
+// const info = (laboratoria) =>{
+//   let processedData = window.data.computeStudentsStats(laboratoria);
+//   // console.log(processedData);
+// };
+
+// const processedData = data.computeStudentsStats(laboratoria)
+
+// ___BOTONES___
+// Funciónes para los botones super califragilística y espialidosos que colapsan en menu Bootstrap
+$('#menu-toggle').click(function(event) {
+  event.preventDefault();
+  $('#wrapper').toggleClass('toggled');
 });
+
+$('#logo-lab').click(function(event) {
+  event.preventDefault();
+  $('#wrapper').toggleClass('toggled');
+});
+
+// Botón de Lima
+$('#lima-toggle').click(function(event) {
+  event.preventDefault();
+  $('lima-gen1').toggleClass('toggled');
+});
+// Botón de México
+
+// Botón de Perú
